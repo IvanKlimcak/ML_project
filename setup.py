7af15eada9149460e8a78f0095a1962bbd86cb08
IvanKlimcak <klimcak.ivan@gmail.com>
2023-10-27 15:32:07 +0200

    adding setup.py and requirements

--- setup.py
+++ setup.py
@@ -1,1 +1,23 @@
 from setuptools import find_packages, setup
+from typing import List 
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(path : str) -> List[str]:
+    
+    requirements = []
+    with open(path, 'r') as f:
+        requirements = f.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+    
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+    name = 'ml package',
+    version = '0.0.1',
+    author = 'Ivan Klimcak',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
